b989a1ab696dc2d9994f64516689d060431a3659
Naida Gachabayova <naishka2005@gmail.com>
2025-12-03 15:28:51 +0400

    logos-changed

--- db.jsx
+++ db.jsx
@@ -1,56 +1,56 @@
 export const partners = [
     {
         icon: (
-            <img src="src/assets/dexscreen-logo.png" alt="dexscreen-logo" className="w-9 h-9" />
+            <img src="/img/dexscreen-logo.png" alt="dexscreen-logo" className="w-9 h-9" />
         ),
         name: "DexScreener",
         desc: "Real-time trading charts"
     },
     {
         icon: (
-            <img src="src/assets/CoinGecko-logo.png" alt="coingecko-logo" className="w-9 h-9" />
+            <img src="/img/CoinGecko-logo.png" alt="coingecko-logo" className="w-9 h-9" />
         ),
         name: "CoinGecko",
         desc: "Price tracking platform"
     },
     {
         icon: (
-            <img src="src/assets/coinmarketcap-logo.png" alt="coinmarketcap-logo" className="w-12 h-12" />
+            <img src="/img/coinmarketcap-logo.png" alt="coinmarketcap-logo" className="w-12 h-12" />
             ),
             name: "CoinMarketCap",
             desc: "Market data aggregator"
     },
     {
         icon: (
-            <img src="src/assets/raydium-logo.png" alt="raydium-logo" className="w-10 h-10" />
+            <img src="/img/raydium-logo.png" alt="raydium-logo" className="w-10 h-10" />
         ),
         name: "Raydium",
         desc: "Decentralized exchange"
     },
     {
         icon: (
-            <img src="src/assets/jupiter-logo.png" alt="jupiter-logo" className="w-10 h-10" />
+            <img src="/img/jupiter-logo.png" alt="jupiter-logo" className="w-10 h-10" />
         ),
         name: "Jupiter",
         desc: "Swap aggregator"
     },
     {
         icon: (
-            <img src="src/assets/phantom-logo.webp" alt="phantom-logo" className="w-13 h-10" />
+            <img src="/img/phantom-logo.webp" alt="phantom-logo" className="w-13 h-10" />
         ),
         name: "Phantom",
         desc: "Crypto wallet"
     },
     {
         icon: (
-            <img src="src/assets/trust-wallet-logo.png" alt="trustwallet-logo" className="w-9 h-9" />
+            <img src="/img/trust-wallet-logo.png" alt="trustwallet-logo" className="w-9 h-9" />
         ),
         name: "Trust Wallet",
         desc: "Mobile wallet"
     },
     {
         icon: (
-            <img src="src/assets/MetaMask-logo.png" alt="metamask-logo" className="w-10 h-10" />
+            <img src="/img/MetaMask-logo.png" alt="metamask-logo" className="w-10 h-10" />
         ),
         name: "MetaMask",
         desc: "Browser wallet"
